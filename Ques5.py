25b7f9b83c34e75ca8019e4b2d3b6393c0f4be96
Mehul sagotia <mehulsagotia@gmail.com>
2025-03-09 10:17:45 +0530

    Create Ques5.py

---
+++ Ques5.py
@@ -0,0 +1,19 @@
+# top k frequent elements
+# Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+      count = {}
+        for num in nums:
+            count[num] = 1 + count.get(num, 0)
+
+        heap = []
+        for num in count.keys():
+            heapq.heappush(heap, (count[num], num))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        res = []
+        for i in range(k):
+            res.append(heapq.heappop(heap)[1])
+        return res
